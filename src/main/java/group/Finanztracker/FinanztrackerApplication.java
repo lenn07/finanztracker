a94e039b5bc3c01ed4543e7d7dbd273f174f530d
Lenn Kanter <lenn.kanterschlimm@gmail.com>
2026-03-27 11:43:43 +0100

    chore: base spring boot structure

---
+++ src/main/java/group/Finanztracker/FinanztrackerApplication.java
@@ -0,0 +1,13 @@
+package group.Finanztracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanztrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanztrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group/Finanztracker/FinanztrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package group.Finanztracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanztrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
